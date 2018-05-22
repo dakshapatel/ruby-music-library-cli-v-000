b24a3bab03e13772945a7ceb6cda39e8f0bf422f
Daksha Patel <dakshapatel100@gmail.com>
2018-05-22 02:12:41 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -29,10 +29,13 @@ class MusicLibraryController
   end
 
   def list_artists
-    #Artist.all.sort_by(&:name).each.with_index(1) do |art,idx|
-    #3puts "#{idx}. #{art.name}"
+    Artist.all.sort_by(&:name).each.with_index(1) do |art,idx|
+    puts "#{idx}. #{art.name}"
+    end
+  end
+
+  def list_artists
      Artist.all.each {|artist| puts artist.name}
-    #end
   end
 
   def list_genres
