e1542408caabae9dfec1f6e82e6e6ed17bb8feef
Daksha Patel <dakshapatel100@gmail.com>
2018-05-22 02:29:49 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -19,7 +19,30 @@ class MusicLibraryController
       puts "What would you like to do?"
 
       input = gets.strip
-      input = gets.list_songs
+
+
+
+      until input == "exit"
+     puts "Please enter action"
+     input = gets.chomp
+     case input
+     when "list songs"
+       list_songs
+    when "list artists"
+       list_artists
+     when "list genres"
+       list_genres
+     when "play song"
+       play_song
+     when "list artist"
+       list_artist
+      when "list genre"
+       list_genre
+      when "exit"
+      puts "Goodbye"
+     else
+       puts "invalid action"
+     end
 
     end
   end
